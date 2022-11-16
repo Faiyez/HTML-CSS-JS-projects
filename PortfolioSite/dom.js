a5623fea7b959125b49a77366ef47737e1eda9c4
Faiyez <smfnoor@mun.ca>
2022-11-16 19:54:10 -0330

    updated skills section

--- PortfolioSite/dom.js
+++ PortfolioSite/dom.js
@@ -1,12 +1,17 @@
 /* Components for front end development */
+var webDevelopment = document.getElementById("webDevelopment");
+webDevelopment.addEventListener("click",addWebDeveloperInfo);
+var SWEDevelopment = document.getElementById("SWEDevelopment");
+SWEDevelopment.addEventListener("click",addSweInfo);
+var skillsDisplay = document.getElementById("skillsDisplay");
 var feContainer = document.getElementById("skillInfo");
 var webDevSrcDown = document.getElementById("webDevSrcDown");
 var frontEnd = document.getElementById("feContainer");
 var testHTML = document.getElementById("testHTML");
-webDevSrcDown.addEventListener("click",addWebDeveloperInfo);
+/*webDevSrcDown.addEventListener("click",addWebDeveloperInfo);*/
 var skills = document.getElementsByClassName("skillItem");
 var webDevSrcUp = document.getElementById("webDevSrcUp");
-webDevSrcUp.addEventListener("click",removeWebDeveloperInfo);
+/*webDevSrcUp.addEventListener("click",removeWebDeveloperInfo);*/
 var htmlSkill = document.createElement('li');
 htmlSkill.setAttribute('id', 'skillId');
 htmlSkill.innerHTML += '<h4 class = "skillItem" id = "item">HTML  <div class="skills html"></div></h4>'; 
@@ -17,14 +22,10 @@ var JSSkill = document.createElement('li');
 JSSkill.setAttribute('id', 'skillId');
 JSSkill.innerHTML += '<h4 class = "skillItem" id = "item">Java Script  <div class="skills JS"></div></h4>'; 
 function addWebDeveloperInfo(e){
-    frontEnd.appendChild(htmlSkill);
-    frontEnd.appendChild(cssSkill);
-    frontEnd.appendChild(JSSkill);
-}
-function removeWebDeveloperInfo(e){
-    frontEnd.removeChild(htmlSkill);
-    frontEnd.removeChild(cssSkill);
-    frontEnd.removeChild(JSSkill);
+    deleteChild();
+    skillsDisplay.appendChild(htmlSkill);
+    skillsDisplay.appendChild(cssSkill);
+    skillsDisplay.appendChild(JSSkill);
 }
 /* Components for back end development */
 var sweContainer = document.getElementById('SWEDevelopment');
@@ -38,11 +39,12 @@ javaSkill.innerHTML += '<h4 class = "skillItem" id = "item">JAVA  <div class="sk
 var pythonSkill = document.createElement("li");
 pythonSkill.setAttribute("id","skillId");
 pythonSkill.innerHTML += '<h4 class = "skillItem" id = "item">Python  <div class="skills html"></div></h4>'; 
-function addSweInfo(e){
-    sweContainer.appendChild(javaSkill);
-    sweContainer.appendChild(pythonSkill);
+function deleteChild(){
+    var container = document.getElementById('skillsDisplay');
+    container.innerHTML = '';
 }
-function removeSweInfo(e){
-    sweContainer.removeChild(javaSkill);
-    sweContainer.removeChild(pythonSkill);
+function addSweInfo(e){
+    deleteChild();
+    skillsDisplay.appendChild(pythonSkill);
+    skillsDisplay.appendChild(javaSkill);
 }
